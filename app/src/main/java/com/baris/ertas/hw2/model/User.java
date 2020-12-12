79059e29d303b0bd547ecc3bef046f7e093acaa8
bariis <ertasbariis@gmail.com>
2020-12-12 13:29:55 +0300

    user model added

---
+++ app/src/main/java/com/baris/ertas/hw2/model/User.java
@@ -0,0 +1,107 @@
+package com.baris.ertas.hw2.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String company;
+    private String city;
+
+    public User(int id, String name, String email, String phone, String company, String city) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.company = company;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    protected User(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        email = in.readString();
+        phone = in.readString();
+        company = in.readString();
+        city = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeString(email);
+        dest.writeString(phone);
+        dest.writeString(company);
+        dest.writeString(city);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+}
